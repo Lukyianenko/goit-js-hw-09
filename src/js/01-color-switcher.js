fc8f5dcf37e164a7d022533fba2f1fe028fc19ea
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2022-12-29 16:30:50 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  const btnStart = document.querySelector('button[data-start]');
+  const btnStop = document.querySelector('button[data-stop]');
+  const bodyEl = document.querySelector('body');
+
+  let timeId = null;
+
+  btnStart.addEventListener('click', onChangeBgColor);
+  btnStop.addEventListener('click', onChangeBgColorSop);
+
+ 
+  function onChangeBgColor() {
+    btnStart.disabled = true;
+    timeId = setInterval(() => { 
+    bodyEl.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+  }
+
+  function onChangeBgColorSop() {
+        clearInterval(timeId);
+        btnStart.disabled = false;
+  }
