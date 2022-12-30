a1cca9bf35093f01c836397b203555730a4d7223
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2022-12-30 14:01:42 +0200

    task 3 step 1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+const formValues = {
+  delay: null,
+  step: null,
+  amount: null,
 }
+
+formEl.addEventListener('submit', addingFormValues);
+formEl.addEventListener('input', addgFormValues);
+
+function addingFormValues(evt) {
+  evt.preventDefault();
+  console.log(formValues);
+}
+
+function addgFormValues(evt) {
+  formValues[evt.target.name] = Number(evt.target.value);
+}
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
